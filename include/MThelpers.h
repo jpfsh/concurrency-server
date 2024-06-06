ef68735dd6084bbdbb72d829908247efc41c2f3a
Julian Puffer <julianspuffer@gmail.com>
2024-06-06 15:52:08 -0700

    misc

--- include/MThelpers.h
+++ include/MThelpers.h
@@ -9,9 +9,7 @@ extern int clientCnt;
 
 // discussion: "helpers you should write "
 // remove_joinable_threads(): Remove all joinable threads from your thread list. See next slide for example using pthread_tryjoin_np.
-
 // kill_and_join_all_threads(): Sends kill to all threads running and then calls pthread_join() on them.
-
 // All your typical linked list functions. Single or double is fine, but double might make removing slightly easier in your remove_joinable_threads() function.
 
 #endif
